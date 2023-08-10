49fa58328b70ed10b19855249cadfb3b0d872ebb
Long Luu <long.luu.ur@renesas.com>
2023-08-10 14:54:42 +0700

    clk: renesas: Add support ETHSS for RZ/T2H
    This patch add clk ETHSS for RZ/T2H
    
    Signed-off-by: Long Luu <long.luu.ur@renesas.com>

--- drivers/clk/renesas/r9a09g077-cpg.c
+++ drivers/clk/renesas/r9a09g077-cpg.c
@@ -164,7 +164,7 @@ static const struct {
 };
 
 static const struct {
-	struct rzt2_mod_clk common[51];
+	struct rzt2_mod_clk common[52];
 } mod_clks = {
 	.common = {
 		DEF_MOD("sci0",         R9A09G077_SCI0_CLK, R9A09G077_PCLKM,
@@ -205,6 +205,8 @@ static const struct {
 					0x310, 16, 0),
 		DEF_MOD("gmac2",	R9A09G077_GMAC2_CLK, R9A09G077_PCLKAH,
 					0x310, 17, 0),
+		DEF_MOD("ethss",	R9A09G077_ETHSS_CLK, R9A09G077_PCLKM,
+					0x310, 3, 0),
 		DEF_MOD("shostif",	R9A09G077_SHOSTIF_CLK, R9A09G077_PCLKH,
 					0x320, 1, 1),
 		DEF_MOD("iic0",		R9A09G077_IIC0_CLK, R9A09G077_PCLKL,
